1a5ac248000314dc16c305ff5f175c92b6754e42
Lana Naesah <lananaesah@gmail.com>
2025-08-30 14:12:16 +0300

    Updated main and ReplyService

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,5 +4,16 @@ package org.example;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        ReplyService service = new ReplyService();
+
+
+        System.out.println(service.reply(null, ReplyType.FORMAL));
+        System.out.println(service.reply("hi", ReplyType.FORMAL));
+        System.out.println(service.reply("hi", ReplyType.FRIENDLY));
+        System.out.println(service.reply("HELLO", ReplyType.CONCISE));
+        System.out.println(service.reply("thanks", ReplyType.FRIENDLY));
+        System.out.println(service.reply("bye", ReplyType.CONCISE));
+        System.out.println(service.reply("what is your name", ReplyType.FORMAL));
+        System.out.println(service.reply("random text", ReplyType.CONCISE));
     }
 }
